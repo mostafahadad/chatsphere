a72b264d54ff87e3ff6678ad5773d45c247335e6
marko <markospyros1@gmail.com>
2024-02-03 11:12:30 +0100

    Fixed the style of PostDisplay, and managed to fix the communication from the API in order to fetch the posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,15 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { listPosts } from "../Services/postService";
 import keycloak from "../keycloak";
+import PostDisplay from "./PostDisplay";
 
 const PostList = () => {
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
-    listPosts(keycloak)
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log("error fetching posts", err));
+    listPosts(keycloak).then((res) => setPosts(res));
   }, []);
 
-  return <div>PostList</div>;
+  return (
+    <div>
+      {posts.map((post, index) => (
+        // Assuming each post has a unique id for a key; otherwise, use the index as a fallback
+        <PostDisplay
+          key={post.id || index}
+          username={post.userId}
+          content={post.content}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default PostList;
